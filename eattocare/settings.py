c3ea0e43a45a3525e63a00b2ac179e08eed70ae3
irina torrealba <irina torrealba>
2026-08-06 00:27:56 -0400

    Añadidos soporte de WhiteNoise para estilos estáticos

--- eattocare/settings.py
+++ eattocare/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- nutrition/apps.py
+++ nutrition/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
-
+import os
 
 class NutritionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'nutrition'
+
+    def ready(self):
+        # Solo intentamos crear el superusuario si estamos en producción y existen las variables
+        if os.environ.get('RENDER'):
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+            username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+            email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'irina.torrealba@gmail.com')
+            password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'rintus-quvnuV-jyqzu7')
+            
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
